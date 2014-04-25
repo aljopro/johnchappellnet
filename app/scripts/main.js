43a11671d8a5e9f2927a40755a3289cf386229de
John Chappell <aljopro@gmail.com>
2014-04-24 21:55:57 -0500

    Added main content to site.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,32 @@
-console.log('\'Allo \'Allo!');
+(function($){
+	$(function(){
+		var isiOS = /(iPad|iPhone|iPod)/g.test( navigator.userAgent );
+		var $window = $(window);
+		var navbar = $('.navbar-transparent');
+		var jumbotron = $('.jumbotron');
+
+		$window.on('scroll', function(event){
+			if($window.scrollTop() > $window.height()){
+				navbar
+					.addClass('navbar-opaque')
+					.removeClass('navbar-transparent');
+			} else {
+				navbar
+					.addClass('navbar-transparent')
+					.removeClass('navbar-opaque');
+			}
+		});
+
+		navbar.find('a').on('click', function(){
+			$(".navbar-toggle:visible").click();
+		});
+
+		if(isiOS){
+			jumbotron.css({
+				"height": "100vh"
+			});
+		}
+
+
+	})
+})(jQuery);
